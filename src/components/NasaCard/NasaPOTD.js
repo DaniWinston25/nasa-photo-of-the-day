338080e0b8021fd01d8675a6718637c3ab1c6b6a
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2020-01-09 21:01:06 -0500

    updates to styles

--- src/components/NasaCard/NasaPOTD.js
+++ src/components/NasaCard/NasaPOTD.js
@@ -1,6 +1,37 @@
 import React, { useState,useEffect } from "react";
 import "./../NasaPOTD.scss";
 import axios from 'axios';
+import styled from 'styled-components';
+
+//styled components for day 2//
+
+
+const Wrapper = styled.div`
+background-color: $alternate;
+padding: 10px 0 20px 0;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;`;
+
+const Title = styled.h1`
+font-size: 3.6rem;
+font-weight: bold;
+color: $dark;
+text-shadow: 2px 2px $light;`
+
+const Date = styled.h3`
+font-size: 2.4rem;`;
+
+const StyledP = styled.p`
+font-size: 1.2rem;
+width: 50%;
+font-family: 'Montserrat';
+color: $dark;
+background-color: $background;
+box-shadow:12px 12px 2px 1px $dark-alt;`;
+
+
 
 
 function NasaPOTD(){
@@ -13,13 +44,19 @@ function NasaPOTD(){
 
     
     return(
-        <div className= 'wrapper'>
-
+        <Wrapper>
+<Title>
   <h1>{data.title}</h1>
+  </Title>
+  <Date>
   <h3 id="date">{data.date}</h3>
+  </Date>
   <img alt = {data.title} src ={data.url}/> 
+  
+  <StyledP>
   <p>{data.explanation}</p>
-        </div>
+  </StyledP>
+        </Wrapper>
     )
 }
 
